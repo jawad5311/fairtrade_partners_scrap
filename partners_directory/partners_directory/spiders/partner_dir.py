35cae398bfdd1a713e00d2c72228293d369aea3a
Jawad <muhammadjawad5311@gmail.com>
2021-12-16 12:28:27 +0500

    selenium req made successfully

--- partners_directory/partners_directory/spiders/partner_dir.py
+++ partners_directory/partners_directory/spiders/partner_dir.py
@@ -1,10 +1,21 @@
+
 import scrapy
+
+from scrapy_selenium import SeleniumRequest
 
 
 class PartnerDirSpider(scrapy.Spider):
     name = 'partner_dir'
-    allowed_domains = ['test.com']
-    start_urls = ['http://test.com/']
+    allowed_domains = ['fairtradecertified.org']
+    start_urls = ['https://partner.fairtradecertified.org/directory/results']
+
+    def start_requests(self):
+        yield SeleniumRequest(
+            url=self.start_urls[0],
+            callback=self.parse
+        )
 
     def parse(self, response):
-        pass
+        print(response)
+        print(response.meta)
+        
