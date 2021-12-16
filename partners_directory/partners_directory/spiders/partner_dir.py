344b6e44e01dc0d4882edb944e85ec2d84d48be4
Jawad <muhammadjawad5311@gmail.com>
2021-12-16 14:43:36 +0500

    wait condition added for profiles visibility

--- partners_directory/partners_directory/spiders/partner_dir.py
+++ partners_directory/partners_directory/spiders/partner_dir.py
@@ -4,6 +4,7 @@ import scrapy
 
 from scrapy_selenium import SeleniumRequest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
@@ -21,21 +22,32 @@ class PartnerDirSpider(scrapy.Spider):
         )
 
     def parse(self, response):
-        print(response)
-        print(response.meta)
+        # Driver object selected from response meta data
+        driver = response.meta['driver']
 
-        titles = response.css('.display-name::text').getall()
+        companies_len = driver.find_elements(By.CLASS_NAME, 'profile')
+        print('COMPANIES LENGTH: ', len(companies_len))
+
+        if len(companies_len) == 25:
+            # Select drop down menu & click 100 results per page
+            drop_down_menu = driver.find_element(By.CSS_SELECTOR, '.page-size-container')
+            drop_down_menu.find_element(By.CSS_SELECTOR, 'a').click()
+            drop_down_menu.find_element(By.CSS_SELECTOR, 'ul li:last-child').click()
+
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.CSS_SELECTOR, '.profile:nth-child(26)')
+                )
+            )
+
+        sel = scrapy.selector.Selector(text=driver.page_source)
+
+        titles = sel.css('.display-name::text').getall()
         titles = [title.strip() for title in titles]
 
         print(len(titles))
         print(titles)
 
-        # Driver object selected from response meta data
-        driver = response.meta['driver']
 
-        # Select drop down menu & click 100 results per page
-        drop_down_menu = driver.find_element(By.CSS_SELECTOR, '.page-size-container')
-        drop_down_menu.find_element(By.CSS_SELECTOR, 'a').click()
-        drop_down_menu.find_element(By.CSS_SELECTOR, 'ul li:last-child').click()
 
         time.sleep(5)
