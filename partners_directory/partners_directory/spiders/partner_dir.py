114436695dc535ec855b0977e677b9920bda576e
Jawad <muhammadjawad5311@gmail.com>
2021-12-16 16:01:30 +0500

    required data scrapped from the profiles

--- partners_directory/partners_directory/spiders/partner_dir.py
+++ partners_directory/partners_directory/spiders/partner_dir.py
@@ -34,20 +34,49 @@ class PartnerDirSpider(scrapy.Spider):
             drop_down_menu.find_element(By.CSS_SELECTOR, 'a').click()
             drop_down_menu.find_element(By.CSS_SELECTOR, 'ul li:last-child').click()
 
-            WebDriverWait(driver, 10).until(
+            WebDriverWait(driver, 60).until(
                 EC.presence_of_element_located(
                     (By.CSS_SELECTOR, '.profile:nth-child(26)')
                 )
             )
 
+        # companies_len = driver.find_elements(By.CLASS_NAME, 'profile')
+
+        # Converting selenium page source to scrapy selector
         sel = scrapy.selector.Selector(text=driver.page_source)
 
-        titles = sel.css('.display-name::text').getall()
-        titles = [title.strip() for title in titles]
+        # Grabbing all the profiles
+        profiles = sel.css('.profile')
+        print('PROFILES LENGTH: ', len(profiles))
 
-        print(len(titles))
-        print(titles)
+        for profile in profiles:
+            title = profile.css('.display-name::text').get().strip()
+            country = profile.css('.country > span::text').get()
+            category = profile.css('.record-type span::text').get()
+            marketing_cat = profile.css('.marketing-categories > span::text').get()
+            FTUSA_id = profile.css('.certs-info div:nth-child(1)::text').get()
+            FLO_id = profile.css('.certs-info div:nth-child(2)::text').get()
+
+            country = self._strip_str(country).upper()
+            category = self._strip_str(category)
+            marketing_cat = self._strip_str(marketing_cat)
+            FTUSA_id = self._strip_str(FTUSA_id)
+            FLO_id = self._strip_str(FLO_id)
+
+
+            print('PROFILE: ', title, country, category, marketing_cat, FTUSA_id, FLO_id)
+
+
+        # titles = sel.css('.display-name::text').getall()
+        # titles = [title.strip() for title in titles]
+        #
+        # print(len(titles))
+        # print(titles)
 
 
 
         time.sleep(5)
+
+    @staticmethod
+    def _strip_str(str_data: str):
+        return str_data.strip().lower() if str_data is not None else ''
