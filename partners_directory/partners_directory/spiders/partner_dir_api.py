4eff623a9dd8fb2db6b387664a4122d0da1fd092
Jawad <muhammadjawad5311@gmail.com>
2021-12-19 22:53:47 +0500

    response recieved with required data

--- partners_directory/partners_directory/spiders/partner_dir_api.py
+++ partners_directory/partners_directory/spiders/partner_dir_api.py
@@ -7,4 +7,9 @@ class PartnerDirApiSpider(scrapy.Spider):
     start_urls = ['https://partner.fairtradecertified.org/directory/account/get?hasProfile=false&page=1&perPage=100']
 
     def parse(self, response):
-        pass
+        profiles = response.json()['accounts']
+        for profile in profiles:
+            print(profile['Marketing_Display_Name__c'])
+        
+
+
