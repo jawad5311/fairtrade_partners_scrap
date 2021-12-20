d99a10bcbf0e344f4bd704c57bfcf4f7a2ecc652
Jawad <muhammadjawad5311@gmail.com>
2021-12-20 12:42:23 +0500

    not able to parse profile data

--- partners_directory/partners_directory/spiders/partner_dir_api.py
+++ partners_directory/partners_directory/spiders/partner_dir_api.py
@@ -1,4 +1,7 @@
+
 import scrapy
+
+from scrapy_selenium import SeleniumRequest
 
 
 class PartnerDirApiSpider(scrapy.Spider):
@@ -16,23 +19,44 @@ class PartnerDirApiSpider(scrapy.Spider):
             flo_id = profile['FLO_ID_Supplier__c']
             account_type = profile['Account_Type__c']
             marketing_cat = profile['Account_Marketing_Categories__c']
+            profile_url = profile['Slug']
 
-            yield {
-                'name': name,
-                'country': country,
-                'ftusa_id': ftusa_id,
-                'flo_id': flo_id,
-                'account_type': account_type,
-                'marketing_cat': marketing_cat,
-            }
+            if profile_url:
+                url = f'https://partner.fairtradecertified.org/profile/{profile_url}'
+                yield SeleniumRequest(
+                    url=url,
+                    callback=self.parse_profile,
+                    meta={
+                        'name': name,
+                        'country': country,
+                        'ftusa_id': ftusa_id,
+                        'flo_id': flo_id,
+                        'account_type': account_type,
+                        'marketing_cat': marketing_cat,
+                }
+                )
+            # else:
+            #     yield {
+            #         'name': name,
+            #         'country': country,
+            #         'ftusa_id': ftusa_id,
+            #         'flo_id': flo_id,
+            #         'account_type': account_type,
+            #         'marketing_cat': marketing_cat,
+            #     }
 
-        if profiles:
-            self.page_num += 1
-            print('PAGE NUMBER: ', self.page_num)
-            next_url = f'https://partner.fairtradecertified.org/directory/account/get?hasProfile=false&page={self.page_num}&perPage=100'
-            yield scrapy.Request(
-                url=next_url,
-                callback=self.parse
-            )
+        # if profiles:
+        #     self.page_num += 1
+        #     print('PAGE NUMBER: ', self.page_num)
+        #     next_url = f'https://partner.fairtradecertified.org/directory/account/get?hasProfile=false&page={self.page_num}&perPage=100'
+        #     yield scrapy.Request(
+        #         url=next_url,
+        #         callback=self.parse
+        #     )
 
+    @staticmethod
+    def parse_profile(response):
+        driver = response.meta['driver']
+        sel = response.replace(body=driver.page_source)
+        print('PROFILE NAME: ', sel.css('.profile-name::text').get())
 
