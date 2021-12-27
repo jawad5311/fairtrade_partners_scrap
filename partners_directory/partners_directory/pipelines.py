6e72bf3a83647703acc245eb5079ec4e9d5062c5
Jawad <muhammadjawad5311@gmail.com>
2021-12-27 11:33:44 +0500

    table created in database for data storage

--- partners_directory/partners_directory/pipelines.py
+++ partners_directory/partners_directory/pipelines.py
@@ -7,7 +7,31 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+import sqlite3
+
 
 class PartnersDirectoryPipeline:
+    def __init__(self):
+        self.curr = sqlite3.connect('fairtrade.db').cursor()
+
+    def create_table(self):
+        self.curr.execute(
+            """DROP TABLE IF EXISTS fairtrade_partners"""
+        )
+        self.curr.execute(
+            """
+                create table fairtrade_partners(
+                    title text,
+                    country text,
+                    account_cat text,
+                    marketing_cat text,
+                    ftusa_id text,
+                    flo_id text,
+                    website text,
+                    phone text,
+                    email text)
+            """
+        )
+
     def process_item(self, item, spider):
         return item
